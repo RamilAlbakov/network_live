82d05a9174686c70748d24f3e9a8ea4ada69c925
Ramil Albakov <ralbakov10@gmail.com>
2022-02-15 21:13:20 +0600

    add tests for enm lte cells

---
+++ tests/test_enm_lte.py
@@ -0,0 +1,23 @@
+"""Test enm lte cells parser."""
+
+
+from network_live.enm.lte_parser import parse_lte_cells
+
+
+def test_parse_lte_cells():
+    """Test enm lte cells parser."""
+    with open('tests/enm_data/lte_cells.txt') as lte_cells_obj:
+        enm_lte_cells = lte_cells_obj.read().split('\n')
+    with open('tests/enm_data/enodeb_id.txt') as enodeb_id_obj:
+        enm_enodeb_ids = enodeb_id_obj.read().split('\n')
+    with open('tests/enm_data/bbu_ip.txt') as bbu_ip_obj:
+        enm_bbu_ips = bbu_ip_obj.read().split('\n')
+
+    cell1, cell2, cell3 = parse_lte_cells(enm_lte_cells, enm_enodeb_ids, enm_bbu_ips, date)
+
+    assert cell1['subnetwork'] == 'LTE_Shymkent'
+    assert cell1['site_name'] == 'ERBS_41428_KAINARFARM_K'
+    assert cell2['earfcndl'] == '6200'
+    assert cell2['qRxLevMin'] == '-124'
+    assert cell3['enodeb_id'] == '500074'
+    assert cell3['ip_address'] == '10.198.39.201'
